538713f680a3a644404260622d18e67adde5bace
kaypohleb <kaleb.is.caleb@gmail.com>
2020-03-06 01:13:39 +0800

    joinLobby wip

--- client/src/components/JoinLobby/JoinLobby.js
+++ client/src/components/JoinLobby/JoinLobby.js
@@ -4,13 +4,34 @@ import {withRouter} from  'react-router-dom';
 //attach to socket.io room here
 class JoinLobby extends Component{
     state={
-        users:''
+        roomInput : 'unfilled',
+        userID:this.props.location.state.userID,
+        idToken: this.props.location.state.idToken
     }
-    componentDidMount = () => {
-        //TODO: get room id
+    inputTextHandler = (event)=>{
+        if(event.target.value.length == 6){
+            this.setState({
+                roomInput : 'filled',
+                roomId:event.target.value
+            })
+        }else{
+            this.setState({
+                roomInput : 'unfilled',
+                roomId:event.target.value
+            })
+        }
     }
-    inputTextHandler = ()=>{
-        
+    checkRoomHandler = ()=>{
+        this.props.history.push({
+            pathname:"/lobby",
+            state:{
+                userID: this.state.id,
+                idToken: this.state.idToken,
+                roomId: this.state.roomId,
+                join:true
+            }
+            
+        });
     }
     render(){
         return (
@@ -19,7 +40,8 @@ class JoinLobby extends Component{
                     <button onClick={()=>{this.props.history.goBack()}}>Back</button>
                 </header>                
                 <input type="text" onChange={this.inputTextHandler}></input>
-                              
+                <h1>{this.state.roomInput}</h1>
+                <button onClick={this.checkRoomHandler}>check</button>
             </div>
         );
     }
